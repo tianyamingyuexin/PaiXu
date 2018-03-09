0b55a99f94e597130616abdbbfe396af169f759c
tianyamingyuexin <3247446295@qq.com>
2018-03-09 18:45:39 +0800

    Initial Commit

---
+++ PaiXu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaiXu
+//
+//  Created by guangshu01 on 2018/3/9.
+//  Copyright © 2018年 guangshu01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaiXu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaiXu
+//
+//  Created by guangshu01 on 2018/3/9.
+//  Copyright © 2018年 guangshu01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
